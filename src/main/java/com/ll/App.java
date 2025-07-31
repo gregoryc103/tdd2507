1c6569f2b7276b36783364e2bf23cb0288074352
gregoryc103 <gregoryc103@gmail.com>
2025-07-31 14:43:11 +0900

    Initial commit: Project setup with README

---
+++ src/main/java/com/ll/App.java
@@ -0,0 +1,8 @@
+package com.ll;
+
+public class App {
+
+    public void run(){
+        System.out.println("Hello World");
+    }
+}
---
+++ src/test/java/com/ll/AppTest.java
@@ -0,0 +1,12 @@
+package com.ll;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+    @Test
+    @DisplayName("테스트1")
+    void test1() {
+        App app = new App();
+    }
+}
